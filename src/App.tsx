a515bb5e3fe018aebf6b061bb08216a376ab9bba
and-gurin <and.gurin@gmail.com>
2023-07-05 00:52:00 +0200

    add newCollection element

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from './layouts/header/Header';
+import NewCollection from "./layouts/new-collection/NewCollection";
 
 function App() {
 
-  return (
-    <>
-      <Header/>
-    </>
-  )
+    return (
+        <>
+            <Header/>
+            <NewCollection/>
+        </>
+    )
 }
 
 export default App
